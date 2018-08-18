bd09279921f8ff884922aa4167d3138c442c98b3
Robert Kacir <kacir@wisc.edu>
2018-08-17 23:56:41 -0500

    Reverse Geocoding
    Searching for an address now launches the get Walkability request. Also, a function now finds the state and city of the search so it can be handed off to the getData.crime function. The structure is really messy. It will need to be cleaned up later.

--- static/js/getData.js
+++ static/js/getData.js
@@ -24,6 +24,7 @@ getData.crime = function () {
         request.send();
     $.getJSON("crime.json", function (json){
         console.log(json);
+        //call whatever call backfunction that will cause things to happen in the interface
     });
     
     // Next is to create a regex to match the requested county info to the JSON records
@@ -32,15 +33,15 @@ getData.crime = function () {
 }
 
 //function retrieves walkability data, this includes isochrone, gjson geomentry and walkability stats
-getData.walkability = function (coorPair) {
+getData.walkability = function () {
     var results;
 	
-	var queryString = $SCRIPT_ROOT + '/walkability/' + coorPair[0] + '/' + coorPair[1];
+	var queryString = $SCRIPT_ROOT + '/walkability/' + lat + '/' + lng;
 	
     console.log("I did it! we made a request to: " + queryString);
 	$.getJSON(queryString , function (data) {
 		console.log('Something Returned from Server');
-		console.log(data);
+        console.log(data);
 	}).fail( function (error) {
 		console.log("server request failed. see error below");
 		console.log(error);
@@ -50,5 +51,5 @@ getData.walkability = function (coorPair) {
 }
 
 //testing to see if connection to flask server works correctly
-getData.walkability([40.73692605118838, -73.99224906926378]);
-getData.crime([40.73692605118838, -73.99224906926378]);
+//getData.walkability([40.73692605118838, -73.99224906926378]);
+//getData.crime([40.73692605118838, -73.99224906926378]);
--- static/js/map.js
+++ static/js/map.js
@@ -4,6 +4,9 @@ var map = {};//makeing the master object for the module
 var lat;
 var lng;
 
+//object which will contain state information about the request and general state of interface element and associated variables
+current = {city : null, state : null};
+
 //creating default lat lng coordinates so the script have something to work with initally. Default is Madison, WI
 map.lat =  43.072457;
 map.lng = -89.401605;
@@ -30,7 +33,35 @@ map.establish = function (){
         collapsed: false,
         errorMessage: "We can seem to find that address. Can you try a different one?"
         
-    }).addTo(map.mymap);
+        })
+        .on("markgeocode" , function(e){
+            //let the lat long coordinates from the result object from the plugin
+            var latlongPar = e.geocode.center;
+            lat = latlongPar.lat;
+            lng = latlongPar.lng;
+
+            //try and get county information from geocoding service
+            
+            $.getJSON("https://nominatim.openstreetmap.org/reverse?format=json&addressdetails=1&lat=" + lat + "&lon=" + lng, function(response){
+                console.log("recieved responce from OSM geocoder, see log below");
+                console.log(response);
+                console.log(response.address.city);
+
+                //assign the attribute information to the current state tracking object so it can be used by many differnt functions
+                //in many different modules inside of the project
+                current.city = response.address.city;
+                current.state = response.address.state;
+
+                //call get crime data now. crime function will use address information taken from global current object
+                //getData.crime();
+            });
+
+            //call each of the datagetter methods and hand them callback functions
+            
+            getData.walkability();
+
+        })
+        .addTo(map.mymap);
     
     map.mymap.on('contextmenu', function(e) {
     console.log(e.latlng.lat + ", " + e.latlng.lng)
